5660b0b10a708673490118638a7c8b186e940969
NAMBUSUNG <46341567+Nam-Busung@users.noreply.github.com>
2022-07-25 09:57:12 +0900

    [FEAT] set video css

---
+++ src/Video.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+  return (
+    <div className="video">
+      <video
+        className="video__player"
+        loop
+        src="http://www.exit109.com/~dnn/clips/RW20seconds_2.mp4"
+      ></video>
+    </div>
+  );
+}
+
+export default Video;
